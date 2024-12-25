9789f0467b6fe3782f23bc6dc4a0215ede667ff8
OmidGharaee <omid.gharaee@hotmail.com>
2024-12-25 17:13:06 +0330

    test

--- 01. Core/UnitOfMeasures.Core.Domain/Unit/Entities/Unit.cs
+++ 01. Core/UnitOfMeasures.Core.Domain/Unit/Entities/Unit.cs
@@ -10,6 +10,8 @@ namespace UnitOfMeasures.Core.Domain.Unit.Entities
 {
     public class Unit : BaseEntity<Guid>
     {
+      
+
         public Unit()
         {
 
@@ -18,6 +20,8 @@ namespace UnitOfMeasures.Core.Domain.Unit.Entities
         public Unit(Guid id)
         {
             Id = id;
+
+
         }
 
         public string Name { get; set; }
---
+++ 02. Infrastructure/UnitOfMeasures.Infrastructure.Persistence/Migrations/20220818120320_ss.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UnitOfMeasures.Infrastructure.Persistence.Migrations
+{
+    public partial class ss : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
